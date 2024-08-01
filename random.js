c95c7f68afc2991c3f15032aaf6fbf840c5a04fe
Fabian Leon <fabianleon1209@gmail.com>
2024-08-01 12:11:21 -0600

    First commit

---
+++ random.js
@@ -0,0 +1,43 @@
+
+'use strict'
+
+
+const range=document.getElementById('customRange2');
+const p= document.getElementById('random');
+const accion=document.getElementById('botonaccion');
+
+
+
+   
+
+   range.addEventListener('change', function() {
+    // Obtener el valor actual del rango
+    const valorActual = range.value;
+    
+    // Hacer algo con el valor actual, por ejemplo, mostrarlo en consola
+    console.log("Nuevo valor del rango:", valorActual);
+    const random = ()=>{
+
+
+        const characters= 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"#$%&/()=¿?*';
+        let result= '';
+        const charactersLength=characters.length;
+    
+        for(let x=0 ; x<valorActual; x++){
+            result+=characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+       
+        return result;
+       
+    };
+    p.innerHTML=random();
+   })
+
+
+
+
+
+
+
+//console.log(random(20));
+//console.log(range);
